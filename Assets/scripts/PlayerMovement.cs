f30d88268dabd0e6190464c3358a376d58ed6d8f
zzenchery <99723552+zzenchery@users.noreply.github.com>
2024-10-06 19:27:18 +0300

    .

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -10,9 +10,11 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField]private GameObject gameObject;
     private GameObject newCube;
+    private Animator anim;
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>(); 
     }
     
     void Update()
@@ -21,6 +23,25 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(directionX * 8f , rb.velocity.y);
         float directionY = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(rb.velocity.x, directionY * 8f);
+        
+        if (directionX > 0f)
+        {
+            anim.SetBool("walking", true);
+        }
+        else if (directionY > 0f)
+        {
+            anim.SetBool("walking", true);
+        }
+        else if (directionX < 0f)
+        {
+            anim.SetBool("walking", true);
+        }
+        else if (directionY < 0f)
+        {
+            anim.SetBool("walking", true);
+        }
+        else
+            anim.SetBool("walking", false);
 
         if(Input.GetButtonDown("Jump"))
         {   
@@ -34,9 +55,9 @@ public class PlayerMovement : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "icecube")
+        if (collision.gameObject.tag == "icecube")
         {
-            if(Input.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump"))
             {
                 Destroy(collision.gameObject);
             }
